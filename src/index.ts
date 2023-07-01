59960ccacb40cf9752fda26ec4c3ebb60d95eadd
OsirisAnubis <olegshulgakalendar@gmail.com>
2023-07-01 16:13:39 +0300

    add test

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hi');
+  res.status(200).send('Hi');
 });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+export { app };
